6b8785cb3853e762778fb51790947cce484766f9
Emanuele Crocicchia <113915081+Leloyea@users.noreply.github.com>
2024-03-04 15:30:37 +0100

    Header.vue and Footer.vue componens added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
 </script>
 
 <template>
+  <Header />
   <RouterView />
+  <Footer />
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<template>
+    <h1>ciao martina</h1>
+</template>
