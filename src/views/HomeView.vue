f04a0b8f338159f7d0c8473d44c86530a3ce76c5
saraobia <sara.obia99@gmail.com>
2024-03-06 15:39:56 +0100

    Home change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,42 +23,49 @@ export default {
 
 <template>
   <!-- DJUMBO -->
-  <div class="flex flex-col">
+  <div class="flex flex-col justify-center items-center md:flex md:flex-row md:px-[50px]">
     <!-- div img/3js -->
-    <div class=" w-[50%] md:flex  md:justify-center md:items-center md:text-[30px]">
-      <img src="/src/assets/Immaginona.png" alt="charmander">
+    <div class="flex justify-center items-center md:flex md:justify-center md:items-center">
+      <img src="/src/assets/Immaginona.png" alt="charmander" class="h-[300px] md:h-[400px] lg:h-[500px]">
     </div>
     <!-- div text -->
-    <div class="w-[50%] flex flex-col justify-start ml-5">
-      <h2 class="font-extrabold text-desktop-xl leading-[40px] mb-4">Welcome to our Vuekémon Store</h2>
-      <p class="font-semibold text-desktop-xs mb-7 pr-[400px]">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <div class="w-[400px] md:w-[300px] lg:w-[400px] border flex flex-col justify-center  md:justify-start md:ml-5">
+      <h1 class=" font-extrabold text-desktop-xl lg:text-[50px] lg:leading-[55px] leading-[40px] mb-2">Welcome to our Vuekémon Store</h1>
+      <p class="font-semibold text-desktop-xs mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Aspernatur
         consequuntur at expedita doloribus inventore accusantium veritatis earum blanditiis.</p>
-      <div>
-        <span class="rounded-md font-semibold bg-[#ce88f0] text-white py-[10px] px-[8px] shadow-xl">Sconto:
-          "Opinno"</span>
-        <span class="rounded-md border-2 border-[#ce88f0] py-[8px] px-[8px] ml-4 shadow-xl">Sconto: "Ciao123"</span>
+
+     <div class="flex">
+        <input type text placeholder=" Search " class="focus:outline-none rounded-xl border-2 border-[#ce88f0] font-semibold text-[#3b3b3b] py-[10px] px-[8px] shadow-xl">
+
+        <!-- <div class="absolute right-20">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+      </div> -->
       </div>
+
+
 
     </div>
   </div>
   <!-- PALLE-CATEGORIE -->
-  <div class="flex justify-center items-center gap-12 my-[130px]">
+  <div class="text-[#3b3b3b] flex justify-center items-center gap-7 mt-[80px] mb-[130px] max-[430px]:flex-col md:gap-12">
     <a href="#peluches">
-        <div class="rounded-full  flex justify-center items-center bg-[#79C5EF] bg-opacity-60 h-[150px] w-[150px] hover:transition delay-150 duration-300 ease-in-out hover:scale-[1.3] hover:text-white hover:bg-[url('/src/assets/img/Gif/Peluche.gif')] hover:bg-cover md:w-[200px] md:h-[200px]">
+        <div class="rounded-full flex justify-center items-center bg-[#79C5EF] bg-opacity-60 h-[125px] w-[125px] hover:transition delay-150 duration-300 ease-in-out hover:scale-[1.3] hover:text-white hover:bg-[url('/src/assets/img/Gif/Peluche.gif')] hover:bg-cover md:w-[200px] md:h-[200px]">
           <span class="font-bold text-[20px]">Peluches</span>
-        </div>  
+        </div>
     </a>
     <a href="#cards">
       <div
-        class="rounded-full flex justify-center items-center bg-[#a1a0df] bg-opacity-60 h-[150px] w-[150px]  hover:transition delay-150 duration-300 ease-in-out hover:scale-[1.3] hover:text-white hover:bg-[url('/src/assets/img/Gif/CardGame.gif')] md:w-[200px] md:h-[200px]">
+        class="rounded-full flex justify-center items-center bg-[#a1a0df] bg-opacity-60 h-[125px] w-[125px]  hover:transition delay-150 duration-300 ease-in-out hover:scale-[1.3] hover:text-white hover:bg-[url('/src/assets/img/Gif/CardGame.gif')] md:w-[200px] md:h-[200px]">
         <span class="font-bold text-[20px]">Cards</span>
       </div>
     </a>
 
     <a href="#games">
       <div
-        class="rounded-full flex justify-center items-center bg-[#CE88F0] bg-opacity-60 h-[150px] w-[150px]  hover:transition delay-150 duration-300 ease-in-out hover:scale-[1.3] hover:text-white hover:bg-[url('/src/assets/img/Gif/GamesRetro.gif')] md:w-[200px] md:h-[200px]">
+        class="rounded-full flex justify-center items-center bg-[#CE88F0] bg-opacity-60 h-[125px] w-[125px]  hover:transition delay-150 duration-300 ease-in-out hover:scale-[1.3] hover:text-white hover:bg-[url('/src/assets/img/Gif/GamesRetro.gif')] md:w-[200px] md:h-[200px]">
         <span class="font-bold text-[20px]">Games</span>
       </div>
     </a>
@@ -66,29 +73,29 @@ export default {
   <div>
     <ul>
       <a name="peluches">
-        <h2 class="font-extrabold text-[35px] ml-5">Peluches</h2>
+        <h2 class="font-extrabold text-[#3b3b3b] text-center text-[35px] mb-[50px] ml-5">Peluches</h2>
       </a>
-      <div class="flex overflow-x-auto py-[50px]">
+      <div class="flex overflow-x-auto pb-[50px]">
         <li class="ml-5" v-for="items in products.peluches">
           <TemplateCard :title="items.title" :price="items.price" :imgSrc="items.img" path="peluche/" :spec="items.name"
             imgClass="w-[150px] h-[200px] rounded-lg" />
         </li>
       </div>
       <a name="cards">
-        <h2 class="font-extrabold text-[35px] ml-5 mt-[90px]">Cards</h2>
+        <h2 class="text-[#3b3b3b] text-center font-extrabold text-[35px] ml-5 mt-[90px] mb-[50px] ">Cards</h2>
       </a>
 
-      <div class="flex overflow-x-auto py-[50px]">
+      <div class="flex overflow-x-auto pb-[50px]">
         <li class="ml-5" v-for="items in products.cards">
           <TemplateCard :title="items.title" :price="items.price" :imgSrc="items.img" path="card/" :spec="items.name"
             imgClass="w-[150px] h-[200px] rounded-lg" />
         </li>
       </div>
       <a name="games">
-        <h2 class="font-extrabold text-[35px] ml-5 mt-[90px]">Games</h2>
+        <h2 class="text-[#3b3b3b] text-center font-extrabold text-[35px] ml-5 mt-[90px] mb-[50px]">Games</h2>
       </a>
 
-      <div class="flex overflow-x-auto py-[50px]">
+      <div class="flex overflow-x-auto pb-[50px]">
         <li class="ml-5" v-for="items in products.games">
           <TemplateCard :title="items.title" :price="items.price" :imgSrc="items.img" path="game/" :spec="items.id"
             imgClass="w-[150px] h-[200px] rounded-lg" />
