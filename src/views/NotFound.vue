f57123bca8c423393b1fcbf8a8ea8488f0c0aa6b
ManuEmme <marascoemanuel1@gmail.com>
2024-03-07 10:57:32 +0100

    final page404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="text-center">
-    <h1 class="text-[5em]">404</h1>
-    <p class="my-4">Sapevi che Pikachu può imparare l'attacco "Tuono" che ha una potenza di 110 ma con il 70% di precisione?</p>
-    <p class="my-4">Qual è il pokémon preferito dai fotografi? Snapdos!</p>
-    <p class="my-4">Charizard può raggiungere l'evoluzione Mega Charizard X o Y, diventando tipo fuoco/drago o mantenendo il tipo fuoco/volante con potenziamenti diversi.</p>
-    <p class="my-4">Bulbasaur è il primo Pokémon nel Pokédex nazionale e può evolversi in Ivysaur.</p>
-    <h2 class="py-[100PX]">Ops! Sembra che tu sia perso... ma sicuramente ora sai qualcosa in più sui pokémon!</h2>
-    <router-link to="/" class="p-4 m-5 bg-blue-500 text-white rounded-full text-lg hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-110">Torna alla home</router-link>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+  <!-- IMG 404 -->
+  
+  <!-- 4POKEBALL4 -->
+  <div class="text-center text-[#3b3b3b] mt-[100px]">
+    <div class="flex justify-center">
+      <h1 class="text-[100px] font-semibold md:text-[150px] lg:text-[200px]">4</h1>
+      <img src="/src/assets/bulbaball1.png" alt="logo" class=" h-[180px] md:h-[300px] lg:h-[400px]">
+      <h1 class="text-[100px] font-semibold md:text-[150px] lg:text-[200px]">4</h1>
+    </div>
+
+
     
+     <div class="font-semibold text-[#3b3b3b] px-[50px] text-[18px] md:text-[20px] lg:text-[30px]">
+      <p class="my-4">Bulbasaur è il primo Pokémon nel Pokédex nazionale e può evolversi in Ivysaur.</p>
+      <p class="my-4">Sapevi che Pikachu può imparare l'attacco "Tuono" che ha una potenza di 110 ma con il 70% di
+      precisione?</p>
+    
+    <p class="my-4">Charizard può raggiungere l'evoluzione Mega Charizard X o Y, diventando tipo fuoco/drago o
+      mantenendo il tipo fuoco/volante con potenziamenti diversi.</p>
+     </div>
+    <h2 class="p-[50px] font-bold text-[25px] md:text-[40px] lg:text-[50px]">Ops! Sembra che tu ti sia perso... ma sicuramente ora sai qualcosa in più sui pokémon!</h2>
+    <router-link to="/"
+      class="p-4 m-5 bg-[#CE88F0] text-white rounded-full text-lg hover:bg-[#79C5EF] transition duration-300 delay-100 ease-in-out transform hover:scale-110">Torna
+      alla home</router-link>
+   
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
   </div>
 </template>
 
